e2fe67787780bab2766546b1f02cf02bc63a6637
John Tsevdos <tsevdosjohn@gmail.com>
2023-08-12 19:01:55 +0300

    feat: add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://phrappe.com",
   compressHTML: true,
-  integrations: [lottie(), tailwind(), mdx()],
+  integrations: [lottie(), tailwind(), mdx(), sitemap()]
 });
