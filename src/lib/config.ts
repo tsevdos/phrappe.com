e199e12df8a1f578b409bf1435009d80b05b7b12
John Tsevdos <tsevdosjohn@gmail.com>
2024-03-15 12:08:01 +0200

    chore: refactor open graph metadata (#10)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,6 @@
 export default {
   title: "Phrappe.com",
   tagline: "ice-chilled, instant web tips",
-  url: "https://phrappe.com",
-  author: "John Tsevdos",
 } as const;
 
 export const POSTS_PER_PAGE = 10;
